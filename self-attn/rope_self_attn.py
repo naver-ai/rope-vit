c6aa201ee795daa4f841e2f9585164bb23a0b819
Byeongho Heo <bh.heo@navercorp.com>
2024-04-29 06:32:50 +0200

    Clean up & bugfix

--- self-attn/rope_self_attn.py
+++ self-attn/rope_self_attn.py
@@ -31,14 +31,12 @@ def init_t_xy(end_x: int, end_y: int):
 
 def compute_mixed_cis(freqs: torch.Tensor, t_x: torch.Tensor, t_y: torch.Tensor, num_heads: int):
     N = t_x.shape[0]
-    depth = freqs.shape[1]
     # No float 16 for this range
     with torch.cuda.amp.autocast(enabled=False):
-        freqs_cis = []
-        freqs_x = (t_x.unsqueeze(-1) @ freqs[0].unsqueeze(-2)).view(depth, N, num_heads, -1).permute(0, 2, 1, 3)
-        freqs_y = (t_y.unsqueeze(-1) @ freqs[1].unsqueeze(-2)).view(depth, N, num_heads, -1).permute(0, 2, 1, 3)
-        freqs_cis.append(torch.polar(torch.ones_like(freqs_x), freqs_x + freqs_y))                    
-    return torch.cat(freqs_cis, dim=-1)
+        freqs_x = (t_x.unsqueeze(-1) @ freqs[0].unsqueeze(-2)).view(N, num_heads, -1).permute(1, 0, 2)
+        freqs_y = (t_y.unsqueeze(-1) @ freqs[1].unsqueeze(-2)).view(N, num_heads, -1).permute(1, 0, 2)
+        freqs_cis = torch.polar(torch.ones_like(freqs_x), freqs_x + freqs_y)
+    return freqs_cis
 
 def compute_axial_cis(dim: int, end_x: int, end_y: int, theta: float = 100.0):
     freqs_x = 1.0 / (theta ** (torch.arange(0, dim, 4)[: (dim // 4)].float() / dim))
@@ -72,6 +70,7 @@ class Attention(nn.Module):
     # taken from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
     def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
         super().__init__()
+        self.dim = dim
         self.num_heads = num_heads
         head_dim = dim // num_heads
         self.scale = qk_scale or head_dim ** -0.5
@@ -109,10 +108,10 @@ class RoPEAttention(Attention):
             self.compute_cis = partial(compute_mixed_cis, num_heads=self.num_heads)
             
             freqs = init_2d_freqs(
-                head_dim=self.dim // self.num_heads, num_heads=self.num_heads, theta=rope_theta, 
+                dim=self.dim // self.num_heads, num_heads=self.num_heads, theta=rope_theta, 
                 rotate=True
-            )
-            self.rope_freqs = nn.Parameter(freqs, requires_grad=True)
+            ).view(2, -1)
+            self.freqs = nn.Parameter(freqs, requires_grad=True)
             
             t_x, t_y = init_t_xy(end_x=14, end_y=14)
             self.register_buffer('freqs_t_x', t_x)
@@ -145,8 +144,6 @@ class RoPEAttention(Attention):
         #########
         
         attn = (q * self.scale) @ k.transpose(-2, -1)
-        
-        attn = (q @ k.transpose(-2, -1))
         attn = attn.softmax(dim=-1)
         attn = self.attn_drop(attn)
 
